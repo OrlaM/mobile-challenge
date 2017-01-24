1cf1bc9829adf31c690ff018bd56923793d0060a
Orla <orla@ConorMs-MacBook-Pro.local>
2017-01-23 19:14:43 -0500

    More clean up

--- MobileChallenge/MobileChallenge/Views/FullScreenPhotoViewCell.swift
+++ MobileChallenge/MobileChallenge/Views/FullScreenPhotoViewCell.swift
@@ -14,10 +14,6 @@ class FullScreenPhotoCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var photoImageView: UIImageView!
     
-    @IBOutlet weak var imageHeightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var imageWidthConstraint: NSLayoutConstraint!
-    @IBOutlet weak var imageAspectRatioConstraint: NSLayoutConstraint!
-
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
@@ -37,23 +33,6 @@ class FullScreenPhotoCollectionViewCell: UICollectionViewCell {
         }
         
         photoImageView.sd_setImage(with: url)
-        
-        // Update image constraints
-        imageAspectRatioConstraint = imageAspectRatioConstraint.setMultiplier(multiplier: CGFloat(photoData.width)/CGFloat(photoData.height))
-
-        if containerView.frame.width < containerView.frame.height {
-            imageWidthConstraint.constant = containerView.frame.width
-            imageHeightConstraint.constant = containerView.frame.width
-        }
-        else {
-            imageWidthConstraint.constant = containerView.frame.height
-            imageHeightConstraint.constant = containerView.frame.height
-        }
-        
-        if photoData.height > photoData.width {
-            imageHeightConstraint.priority = 950
-            imageWidthConstraint.priority = 900
-        }
         
         self.containerView.layoutIfNeeded()
     }
